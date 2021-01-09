e6e5dbee0fe5241fd313a04f4f9e34a89a9d54ee
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-01-09 11:24:34 +0900

    init

---
+++ StudyCoreData/StudyCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCoreDataApp.swift
+//  StudyCoreData
+//
+//  Created by Shohei Yamamoto on 2021/01/09.
+//
+
+import SwiftUI
+
+@main
+struct StudyCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
